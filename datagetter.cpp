8a1c4c19a40ef169d46b09139f0decd43759eaa2
RomanPC <ludoviksoso@gmail.com>
2023-06-07 01:52:36 +0700

    Получение данных использует паттерн Стратегия. Добавлен JSON - формат

--- datagetter.cpp
+++
@@ -1,2 +0,0 @@
-#include "datagetter.h"
-
--- datagetter.h
+++ datagetter.h
@@ -8,7 +8,7 @@
 #include <QFileSystemModel>
 #include <QTreeView>
 #include <QTableView>
-#include "ui_mainwindow.h"
+
 #include <QSplitter>
 #include <QListView>
 #include <QTreeView>
@@ -18,42 +18,42 @@
 #include <QHeaderView>
 #include <QStatusBar>
 
-// Подключаем библиотеки для работы с БД
+// Подключаем библиотеки для работы с SQLITE
 #include <QtSql>
 #include <QSqlQueryModel>
 
-// Подключаем библиотеки для работы с графиками
-#include <QtCharts/QChartView>
-#include <QtCharts/QPieSeries>
-#include <QtCharts/QPieSlice>
-#include <QtCharts>
-#include <QPainter>
-#include <QPdfWriter>
+// Подключаем библиотеки для работы с JSON
 
-class IDataGetter
+
+
+class IDataGetterStrategy
 {
 public:
-    virtual ~IDataGetter() = default;
+    virtual ~IDataGetterStrategy() = default;
     virtual bool CheckFile(const QString &filePath) = 0;
-    virtual QList<QPair<QString, qreal>> getData(const QString &filePath) = 0;
+    virtual QList<QPair<QString, qreal>> GetData(const QString &filePath) = 0;
 
 };
 
-class SQLiteDataGetter : public IDataGetter
+class SQLiteDataGetterStrategy : public IDataGetterStrategy
 {
     bool CheckFile(const QString &filePath)
     {
-        closeDatabase();
         openDatabase(filePath);
         QStringList tables = database.tables();
         if (tables.isEmpty()) {
             qDebug() << "В базе данных отсутствуют таблицы";
-            return 0;
+            closeDatabase();
+            return false;
         }
-        return 1;
+        closeDatabase();
+        return true;
     }
 
-    QList<QPair<QString, qreal>> getData(const QString &filePath)
+    // Пока что за процесс компоновки данных отвечает функция получения данных
+    // Так мы обрабатываем данные при их получении и занимаем меньше пространства в памяти при хранении этих данных
+    // Но стоит ли компоновку данных выносить в отдельную функцию?
+    QList<QPair<QString, qreal>> GetData(const QString &filePath)
     {
         closeDatabase();
         openDatabase(filePath);
@@ -61,59 +61,98 @@ class SQLiteDataGetter : public IDataGetter
         QStringList tables = database.tables();
         QString tableName = tables.first();
         QSqlQuery query;
-        if (!query.exec    ("SELECT substr(Time, 7, 4) || '-' || substr(Time, 4, 2) AS month_year, AVG(Value) AS average "
-                        "FROM " + tableName + " "
-                                      "GROUP BY month_year"))
-        {
-            qDebug() << "Не удалось выполнить запрос";
-        }
+        // пока что будем считать, что запрос выполняется в любом случае
+        query.exec("SELECT * FROM " + tableName + " "); // предусмотреть sql инъекцию?
         while (query.next()) {
-            QString monthYear = query.value(0).toString();
-            qreal average = query.value(1).toReal();
-            data.append(qMakePair(monthYear, average));
+            QString Time = query.value(0).toString();
+            qreal Value = query.value(1).toReal();
+            data.append(qMakePair(Time, Value));
         }
+        closeDatabase();
         return data;
+    }
+
+private:
+    QSqlDatabase database;
+
+    void openDatabase(const QString& filePath)
+    {
+        closeDatabase();
+        database = QSqlDatabase::addDatabase("QSQLITE");
+        database.setDatabaseName(filePath);
+
+        if (!database.open())
+        {
+                qDebug() << "Ошибка открытия базы данных:" << database.lastError().text();
+        }
     }
 
     void closeDatabase()
     {
         if (database.isOpen())
         {
-            database.close();
-            database.removeDatabase(database.connectionName());
+                database.close();
         }
     }
 
-    bool openDatabase(const QString& filePath)
+};
+
+
+
+class JSONDataGetterStrategy : public IDataGetterStrategy
+{
+public:
+    bool CheckFile(const QString& filePath)
     {
-        closeDatabase();
-
-        database = QSqlDatabase::addDatabase("QSQLITE");
-        database.setDatabaseName(filePath);
-
-        if (!database.open())
+        QFile file(filePath);
+        if (!file.open(QIODevice::ReadOnly))
         {
-            qDebug() << "Ошибка открытия базы данных:" << database.lastError().text();
+                qDebug() << "Не удалось открыть файл:" << filePath;
+                return false;
+        }
+
+        QByteArray jsonData = file.readAll();
+        file.close();
+
+        QJsonDocument jsonDoc = QJsonDocument::fromJson(jsonData);
+        if (jsonDoc.isNull() || !jsonDoc.isArray())
+        {
+                qDebug() << "Данный файл JSON некорректен";
+                return false;
+        }
+
+        QJsonArray jsonArray = jsonDoc.array();
+        if (jsonArray.isEmpty())
+        {
+                qDebug() << "Данный JSON файл пуст!";
                 return false;
         }
 
         return true;
     }
 
-private:
-    QSqlDatabase database;
-
+    QList<QPair<QString, qreal>> GetData(const QString& filePath)
+    {
+        QFile file(filePath);
+        file.open(QIODevice::ReadOnly);
+        QByteArray jsonData = file.readAll();
+        file.close();
+        QJsonDocument jsonDoc = QJsonDocument::fromJson(jsonData);
+        QJsonArray jsonArray = jsonDoc.array();
+        QList<QPair<QString, qreal>> data;
+        for (const QJsonValue& value : jsonArray)
+        {
+                if (value.isObject())
+                {
+                    QJsonObject obj = value.toObject();
+                    QString Time = obj["Time"].toString();
+                    qreal Value = obj["Value"].toDouble();
+                    data.append(QPair<QString, qreal>(Time, Value));
+                }
+        }
+        return data;
+    }
 };
-
-
-
-//class IDataGetter
-//{
-//public:
-//    virtual ~IDataGetter() = 0;
-//    virtual QList<QPair<QString, qreal>> getData() const = 0;
-
-//};
 
 //class IDataGetter
 //{
